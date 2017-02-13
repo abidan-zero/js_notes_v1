3b956c830784f69bc0df28afa2dc50f06373c934
ab7code <abidan.r@gmail.com>
2017-02-13 03:04:50 +0000

    inserted updates from codepen.io so hopefully last time :)"

--- assets/js.js
+++ assets/js.js
@@ -1,3 +1,9 @@
+var notesList = new Object();
+// Pre-defined notes in list
+notesList[1]={title:'1st note',content:'note 1 content info'}
+notesList[2]={title:'2nd note',content:'note 2 content info'}
+notesList[3]={title:'3rd note',content:'note 3 content info'}
+
 $('#main_menu').click(function(){
   //alert('test');
   $('#sub_menu_a').toggle();
